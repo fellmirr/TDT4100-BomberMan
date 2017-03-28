cb3c399f8d8151808dd378ff10ffdd927bf8efd2
Henfau <henrik.fauskanger@gmail.com>
2017-03-29 00:03:07 +0200

    Test av git

---
+++ src/application/SimpleClass.java
@@ -0,0 +1,12 @@
+package application;
+
+public class SimpleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is a simple class that does something.");
+
+	}
+
+}
